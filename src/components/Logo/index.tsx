3dc7bd554a4f081887240945774dfdd7528f5e4f
Colette Ulianogit config --global --listclear <culiano@live.com>
2024-04-25 21:26:49 -0400

    Logo component WIP

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,24 +1,25 @@
 // TODO
-// prop for path to logo file
-// prop for logo text if no logo file
-// prop for target _blank
+// //prop for path to logo file. If it doesn't exist display altText. Maybe just do this instead of a separate prop for logo text
+// //prop for logo text if no logo file
+// //prop for target _blank
 // prop to size the logo?
-// prop to useLink
-// if useLink = true then it's Link and to, else anchor and href
+// //prop to useLink
+// //if useLink = true then it's Link and to, else anchor and href
+// prop for the link
 
 import { Link } from "react-router-dom";
 
-const Logo = (props: { path?: string; altText: string; useLink?: boolean }) => {
+const Logo = (props: { urlLink: string; altText: string; useLink?: boolean; newTab?: boolean; logoFile?: string }) => {
     return (
         <>
             {props.useLink ? (
                 <Link className="navbar-brand" to="/">
+                    {props.logoFile ? <img src={props.logoFile} alt={props.altText} /> : props.altText}
                     {/* <img src="/assets/logo-slate.png" alt="" /> */}
-                    Navbar
                 </Link>
             ) : (
-                <a className="navbar-brand" href="#">
-                    Navbar
+                <a className="navbar-brand" href="#" target={props.newTab ? "_blank" : undefined}>
+                    {props.logoFile ? <img src={props.logoFile} alt={props.altText} /> : props.altText}
                 </a>
             )}
         </>
