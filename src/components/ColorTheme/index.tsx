a77316a253114ec5d3a470d2f56de4108f72330c
Colette Ulianogit config --global --listclear <culiano@live.com>
2024-04-25 20:52:37 -0400

    continuing with advanced ColorTheme

--- src/components/ColorTheme/index.tsx
+++ src/components/ColorTheme/index.tsx
@@ -1,62 +1,16 @@
 // TODO
-// // so this button, when clicked, needs to target the data-bs-theme attribute in the html tag and change it to dark/light
-// // const rootElement = document.documentElement; To select the <html>
-// // rootElement.getAttribute(); - to get the attribute
-// // rootElement.setAttribute(); - to set the attribute
-// // rootElement.removeAttribute(); - to remove the attribute
-// // add props to customize or remove the button text
-// // add props to set starting theme
+// get theme prop working
+//// get text prop working
+//// get textVisible prop working
+// get color switching to work with the new drop down
+// figure out how to get the auto option to work
 
 import { useState, useEffect } from "react";
 
 const ColorTheme = (props: { theme?: string; textVisible?: boolean; text?: string }) => {
-    // ? longer way to write this all out, just put props.theme || "dark" right into the useState as it's value
-    // const initialTheme = props.theme || "dark";
-    // const [theme, setTheme] = useState(initialTheme);
-    // ? seemed redundant to have the value here and repeated again in the useEffect
-    // const [theme, setTheme] = useState(props.theme || "dark");
-    // const [text, setText] = useState(props.text || "Change Theme");
     const [theme, setTheme] = useState("");
     const [text, setText] = useState("");
 
-    // * first go - setting/changing theme
-    // const rootElement = document.documentElement;
-    // rootElement.setAttribute("data-bs-theme", theme);
-
-    // const toggleTheme = () => {
-    //     const newTheme = theme === "dark" ? "light" : "dark";
-    //     rootElement.setAttribute("data-bs-theme", newTheme);
-    //     setTheme(newTheme);
-    // };
-
-    // * second go - setting/changing theme
-    // useEffect(() => {
-    //     const rootElement = document.documentElement;
-    //     rootElement.setAttribute("data-bs-theme", theme);
-    // }, [theme]);
-
-    // const toggleTheme = () => {
-    //     // ? longer way to write this all out
-    //     // const newTheme = theme === "dark" ? "light" : "dark";
-    //     // setTheme(newTheme);
-    //     setTheme(theme === "dark" ? "light" : "dark");
-    // };
-
-    // * third go - setting/changing theme. Completely reactive, changes show without refreshing page
-    useEffect(() => {
-        setTheme(props.theme || "dark");
-    }, [props.theme]);
-
-    useEffect(() => {
-        const rootElement = document.documentElement;
-        rootElement.setAttribute("data-bs-theme", theme);
-    }, [theme]);
-
-    const toggleTheme = () => {
-        setTheme(theme === "dark" ? "light" : "dark");
-    };
-
-    // * first go - setting/changing text. Based off of what was done for changing/setting theme. Completely reactive, changes show without refreshing page
     useEffect(() => {
         setText(props.text || "Change Theme");
     }, [props.text]);
@@ -70,39 +24,36 @@ const ColorTheme = (props: { theme?: string; textVisible?: boolean; text?: strin
                     type="button"
                     aria-expanded="false"
                     data-bs-toggle="dropdown"
-                    data-bs-display="static"
-                    aria-label="Toggle theme (dark)">
+                    data-bs-display="static">
                     <i className="bi bi-moon-fill"></i>
-                    <span className="d-lg-none ms-2" id="bd-theme-text">
-                        Toggle theme
-                    </span>
+                    {props.textVisible !== false && (
+                        <span className="ms-2" id="bd-theme-text">
+                            {text}
+                        </span>
+                    )}
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
                     <li>
                         <button type="button" className="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-                            <i className="bi bi-moon-fill"></i>
+                            <i className="bi bi-sun-fill"></i>
                             Light
-                            <i className="bi bi-check2"></i>
+                            {/* <i className="d-none bi bi-check2"></i> */}
                         </button>
                     </li>
                     <li>
-                        <button
-                            type="button"
-                            className="dropdown-item d-flex align-items-center active"
-                            data-bs-theme-value="dark"
-                            aria-pressed="true">
+                        <button type="button" className="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="true">
                             <i className="bi bi-moon-fill"></i>
                             Dark
-                            <i className="bi bi-check2"></i>
+                            {/* <i className="d-none bi bi-check2"></i> */}
                         </button>
                     </li>
-                    <li>
+                    {/* <li>
                         <button type="button" className="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="false">
                             <i className="bi bi-moon-fill"></i>
                             Auto
-                            <i className="bi bi-check2"></i>
+                            <i className="d-none bi bi-check2"></i>
                         </button>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         </>
