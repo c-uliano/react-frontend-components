c64193a1ffcd034fcf56371e86a6448cdc1024f1
Colette Ulianogit config --global --listclear <culiano@live.com>
2024-05-08 22:19:04 -0400

    logo prop added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         <header className="border-bottom border-primary">
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <Logo altText="The Logo" url="#" />
+                    <Logo altText="The Logo" url="#" logoFile="../../src/assets/logo-dark.png" />
                     <button
                         className="navbar-toggler"
                         type="button"
