1ccb91659d594eaa279855e3b34e77376d3ab07f
Coletta Uliano <culiano@live.com>
2024-07-10 20:24:51 +0000

    Hero component updates

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,41 +1,29 @@
 // TODO this is pulled directly from an old project, needs to be updated
 // move objects into util.ts
     // wait, may not need util.ts
-// add the classes to the appropriate divs, import the module.scss
+// //add the classes to the appropriate divs, import the module.scss
+    // or you could add all the cooresponding bootstrap classes that would do the same thing, but that's a lot of classes
 // prop to conditionally render Link or anchor for the button
 // this should eventually be a Button component
+// expand all current props to be typescripted up
+
+// ! I don't remember why the jsx is structured like this, need to look at the example project
+
+import styles from "./hero.module.scss";
 
 const Hero = (props: any) => {
-    // TODO: a lot of these can be put in a class, only need the props ones to be here
     const bgHero: any = {
-        position: "relative",
-        backgroundColor: "#000000",
-        mixBlendMode: "multiply",
-        height: `${props.height}`,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
+        height: `${props.height}`
     };
 
-    // TODO: a lot of these can be put in a class, only need the props ones to be here
     const bgHeroBefore: any = {
-        content: "",
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
         backgroundImage: `url(${props.image})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: `${props.bgPosition}`,
-        opacity: 0.4,
-        zIndex: -1
+        backgroundPosition: `${props.bgPosition}`
     }
 
     return (
-        <div className="px-4 py-5 text-center" style={bgHero}>
-            <div style={bgHeroBefore}></div>
+        <div className={`px-4 py-5 text-center ${styles.bgHero}`} style={bgHero}>
+            <div className={styles.bgHeroBefore} style={bgHeroBefore}></div>
             <div className="py-5 w-100">
                 <h1 className="display-5 fw-bold text-white mt-2">{props.h1Header}</h1>
                 <div className="col-lg-6 mx-auto">
