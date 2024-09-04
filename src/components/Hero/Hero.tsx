86c426c26870db85fe682e3854c6ccda2663f332
Coletta Uliano <culiano@live.com>
2024-09-04 18:34:55 +0000

    work on Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,4 @@
 // TODO this is pulled directly from an old project, needs to be updated
-// move objects into util.ts
-// wait, may not need util.ts
 // //add the classes to the appropriate divs, import the module.scss
 // or you could add all the cooresponding bootstrap classes that would do the same thing, but that's a lot of classes
 // prop to conditionally render Link or anchor for the button
@@ -8,7 +6,7 @@
 // this should eventually be a Button component
 // expand all current props to be typescripted up
 
-// ! I don't remember why the jsx is structured like this, need to look at the example project
+// //! I don't remember why the jsx is structured like this, need to look at the example project
 
 // TODO:
 // //prop for height?, string
@@ -25,17 +23,17 @@ import { HeroProps } from "./utils";
 // // TODO: can this long list of props be put in the utils folder and just use an interface here?
 const Hero = (props: HeroProps) => {
     const bgHero: object = {
-        height: props.height || "50vh",
+        height: props.height || "50vh"
     };
-    // TODO: bgPosition default of center center?
-    const bgHeroBefore: any = {
+    // //TODO: bgPosition default of center center?
+    const bgHeroBefore: object = {
         backgroundImage: `url(${props.imgUrl})`,
         backgroundPosition: props.bgPosition || "center center",
     };
 
     return (
-        <div className={`px-4 py-5 text-center ${styles.bgHero}`} style={bgHero}>
-            <div className={styles.bgHeroBefore} style={bgHeroBefore}></div>
+        <div className={`px-4 py-5 text-center ${styles.bgHero}`} style={{...bgHero}}>
+            <div className={styles.bgHeroBefore} style={{...bgHeroBefore}}></div>
             <div className="py-5 w-100">
                 <h1 className="display-5 fw-bold text-white mt-2">{props.h1Header}</h1>
                 <div className="col-lg-6 mx-auto">
