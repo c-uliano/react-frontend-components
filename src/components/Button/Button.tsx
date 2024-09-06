85b9f898c4f836ba8094bc7743cd8f84f112043e
Coletta Uliano <culiano@live.com>
2024-09-06 17:41:46 +0000

    setting up utils.ts, starting on Button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,25 @@
+import { ButtonProps } from "./utlis";
 
 
-const Button = () => {
+const Button = (props: ButtonProps) => {
+
+    const renderButton = () => {
+        switch (props.btnType) {
+            case 'button':
+                return <button onClick={props.onClick} type="button" className="btn">{props.text}</button>;
+            default:
+                return <button onClick={props.onClick} type="button" className="btn">{props.text}</button>
+        }
+    }
+
     return (
         <>
-            <button type="button" className="btn">A Button</button>
+            {renderButton()}
+            {/* <button onClick={props.onClick} type="button" className="btn">{props.text}</button> */}
+
+            {/* {props.btnType == 'button' && (
+
+            ) } */}
         </>
     )
 }
--- src/components/Button/utlis.ts
+++ src/components/Button/utlis.ts
@@ -1,7 +1,18 @@
+type ButtonTypes = 'Link' | 'input' | 'button' | 'anchor';
+
+type InputTypes = 'input' | 'submit' | 'reset';
+
+type IconPosition = 'start' | 'end';
+
 export interface ButtonProps {
     text?: string,
-    onClick?: Function,
+    onClick?: () => void, // do this instead of Function
     size?: string,
     variant?: string,
-    href?: string, 
+    url?: string, 
+    btnType?: ButtonTypes,
+    type?: InputTypes,
+    icon?: string, // if this is truthy then an icon displays
+    iconPosition: IconPosition,
+    hideText?: boolean
 }
