b99b62773fe57fca05ada795a468df39fc4d03f5
Coletta Uliano <culiano@live.com>
2024-10-29 14:50:13 +0000

    continuing with Button component

--- src/components/Button/utlis.ts
+++ src/components/Button/utlis.ts
@@ -4,16 +4,18 @@ type InputTypes = 'input' | 'submit' | 'reset';
 
 type IconPosition = 'start' | 'end';
 
+type buttonVariants = 'btn-primary' | 'btn-secondary' | 'btn-success' | 'btn-danger' | 'btn-warning' | 'btn-info' | 'btn-light' | 'btn-dark' | 'btn-link';
+
 export interface ButtonProps {
     text?: string,
     onClick?: () => void, // do this instead of Function
     size?: string,
-    variant?: string,
+    variant?: buttonVariants, // //TODO: update this to a type with the Bootstrap button class options
     url?: string, 
     btnType?: ButtonTypes,
     type?: InputTypes,
     icon?: string, // if this is truthy then an icon displays
-    iconPosition: IconPosition,
+    iconPosition?: IconPosition,
     hideText?: boolean,
     newTab?: boolean
 }
